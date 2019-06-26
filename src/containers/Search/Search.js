a7223417bc8f8b62cf07fc465aa99290ce7ef72c
Rakesh Jain <rakeshvora2007@gmail.com>
2019-06-26 12:45:54 -0400

    search input and button styled

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,42 +1,54 @@
-import React, { /* useEffect, */ useState} from 'react';
-import {APP_CONFIG} from '../../config/Keys';
+import React, { /* useEffect, */ useState } from "react";
+import { APP_CONFIG } from "../../config/Keys";
+import styles from "./Search.module.css";
 
-export const Search = (props) => {
+export const Search = props => {
+  const [searchTerm, setSearchTerm] = useState("");
+  // const [recipes, setRecipes] = useState([]);
 
-    const [searchTerm, setSearchTerm] = useState("");
-    // const [recipes, setRecipes] = useState([]);
-
-    /* useEffect(() => {
+  /* useEffect(() => {
         getRecipes()
     }, []); */
-    
-    const getRecipes = async (query, fxn) => {
-        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_CONFIG.APP_ID}&app_key=${APP_CONFIG.APP_KEY}`);
-        const data = await response.json();
-        fxn(data);
-    }
 
-    const onSearch = (e) => {
-        e.preventDefault();
-        console.log(searchTerm)
-        if(searchTerm !== '') {
-            getRecipes(searchTerm, (listOfRecipes) => {
-                // setRecipes(listOfRecipes);
-                props.handleRecipesUpdate(listOfRecipes.hits);
-            })
-        } else {
-            alert("Please enter a value before hitting submit button");
-        }
-    }
+  const getRecipes = async (query, fxn) => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${
+        APP_CONFIG.APP_ID
+      }&app_key=${APP_CONFIG.APP_KEY}`
+    );
+    const data = await response.json();
+    fxn(data);
+  };
 
-    const onChangeSearchTerm = (value) => {
-        setSearchTerm(value);
+  const onSearch = e => {
+    e.preventDefault();
+    console.log(searchTerm);
+    if (searchTerm !== "") {
+      getRecipes(searchTerm, listOfRecipes => {
+        // setRecipes(listOfRecipes);
+        props.handleRecipesUpdate(listOfRecipes.hits);
+      });
+    } else {
+      alert("Please enter a value before hitting submit button");
     }
+  };
 
-    return (
-        <form onSubmit={onSearch}>
-        <input type="text" value={searchTerm} onChange={e => onChangeSearchTerm(e.target.value)}/>
-        <input type="submit" value="Submit"/>
-        </form>
-    )
-}
+  const onChangeSearchTerm = value => {
+    setSearchTerm(value);
+  };
+
+  return (
+    <div className={styles.form}>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchTerm}
+          placeholder="Search"
+          className={styles.inputText}
+          onChange={e => onChangeSearchTerm(e.target.value)}
+        />
+        <input type="submit" value="Submit" className={styles.inputSubmit}/>
+      </form>
+    </div>
+  );
+};
