6b5a56e8183f32e881b7b6530250370d539178c3
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-02 18:51:00 -0400

    added calories to each recipe

--- src/containers/Recipe/RecipeList/RecipeList.js
+++ src/containers/Recipe/RecipeList/RecipeList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './RecipeList.module.css'
 
-export const RecipeList = ({image, label, ingredientLines}) => {
+export const RecipeList = ({image, label, ingredientLines, calories}) => {
     return (
         <div className={styles.recipeItem}>
             <h3 className={styles.recipeItemTitle}>{label}</h3>
@@ -9,6 +9,7 @@ export const RecipeList = ({image, label, ingredientLines}) => {
             <ul className={styles.recipeIngridientList}>{ingredientLines.map((line, index) => {
                 return <li key={index}>{line}</li>
             })}</ul>
+            <h3>Calories: {calories}</h3>
         </div>
     );
 }
