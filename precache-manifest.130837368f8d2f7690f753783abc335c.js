b2d4709314f0e64beecb48095de9370aa04e3eed
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-24 12:50:19 -0400

    Updates

---
+++ precache-manifest.130837368f8d2f7690f753783abc335c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05948ff2fa23c917cff3a77a0e26bdfd",
+    "url": "/recipe-search-using-react/index.html"
+  },
+  {
+    "revision": "13b100b4102e6e290679",
+    "url": "/recipe-search-using-react/static/css/main.d0a91744.chunk.css"
+  },
+  {
+    "revision": "2b267f1367f929a28ca9",
+    "url": "/recipe-search-using-react/static/js/2.bf9f4d95.chunk.js"
+  },
+  {
+    "revision": "13b100b4102e6e290679",
+    "url": "/recipe-search-using-react/static/js/main.a6863df9.chunk.js"
+  },
+  {
+    "revision": "f4d3b69e4db54aef5c30",
+    "url": "/recipe-search-using-react/static/js/runtime~main.cae0aeb7.js"
+  }
+]);
