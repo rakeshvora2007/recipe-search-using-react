60f4b5818544ec3d9455a22b8e04051ded6c6291
Rakesh Jain <rakeshvora2007@gmail.com>
2020-09-09 18:59:26 -0400

    recipe card styles

--- src/containers/Recipe/RecipeList/RecipeList.js
+++ src/containers/Recipe/RecipeList/RecipeList.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from './RecipeList.module.css'
 
-export const RecipeList = ({image, label, ingredientLines, calories}) => {
+export const RecipeList = ({ image, label, ingredientLines, calories }) => {
     return (
         <div className={styles.recipeItem}>
-            <h3 className={styles.recipeItemTitle}>{label}</h3>
-            <img src={image} alt="" className={styles.recipeImage}/>
-            <ul className={styles.recipeIngridientList}>{ingredientLines.map((line, index) => {
-                return <li key={index}>{line}</li>
-            })}</ul>
-            <h3>Calories: {calories}</h3>
+            
+            <div className={styles.recipeImageContainer}>
+                <img src={image} alt="" className={styles.recipeImage} />
+            </div>
+            <div className={styles.header}>
+                <div className={styles.recipeItemTitle}>{label} <sup className={styles.calories}>{Math.round(calories)} Cal</sup></div>
+            </div>
+            <div className={styles.ingredientsContainer}>
+                <ul className={styles.recipeIngridientList}>{ingredientLines.map((line, index) => {
+                    return <li key={index}>{line}</li>
+                })}</ul>
+            </div>
         </div>
     );
 }
